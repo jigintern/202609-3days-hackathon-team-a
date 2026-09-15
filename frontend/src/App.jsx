3abd9939577cb2bf25b734fef63417397329c849
rekku0624 <kamoryo624@gmail.com>
2026-09-15 16:09:36 +0900

    アーティスト詳細ページを追加し、アーティスト情報とイベントを表示する機能を実装

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './pages/Login.jsx'
 import Signup from './pages/Signup.jsx'
 import ProfileSetup from './pages/ProfileSetup.jsx'
 import Artists from './pages/Artists.jsx'
+import ArtistDetail from './pages/ArtistDetail.jsx'
 import ProtectedRoute from './components/ProtectedRoute.jsx'
 
 function App() {
@@ -25,6 +26,14 @@ function App() {
         element={
           <ProtectedRoute>
             <Artists />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/artists/:artistId"
+        element={
+          <ProtectedRoute>
+            <ArtistDetail />
           </ProtectedRoute>
         }
       />
